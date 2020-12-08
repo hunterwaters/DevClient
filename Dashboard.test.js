5a4020919dade3e69591c89528638746a24dd426
hunterwaters <55896961+hunterwaters@users.noreply.github.com>
2020-12-08 15:00:48 -0800

    Delete Dashboard.test.js

--- Dashboard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DashboardActions from './DashboardActions';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<DashboardActions />, div)
-})
